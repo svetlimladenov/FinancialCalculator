fb170b7a8970c6c424f5fab02f95e495641db76d
Svetlin <svetlimladenov@gmail.com>
2021-01-13 16:31:37 +0200

    Add Api project with swagger ui

---
+++ src/StateMachineDemo/Api/Controllers/CreditController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CreditController : ControllerBase
+    {
+        private readonly ILogger<CreditController> _logger;
+
+        public CreditController(ILogger<CreditController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("UtilizeCredit")]
+        public IActionResult UtilzeCredit()
+        {
+            return Ok("Raboti");
+        }
+    }
+}
