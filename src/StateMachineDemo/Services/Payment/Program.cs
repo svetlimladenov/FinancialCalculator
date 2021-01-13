b2245b3bf5938dcfa2ce760e1095b4936b9499c5
Svetlin <svetlimladenov@gmail.com>
2021-01-13 16:35:47 +0200

    Add Service projects with installed packages

---
+++ src/StateMachineDemo/Services/Payment/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Payment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hostBuilder = new HostBuilder();
+        }
+    }
+}
