7f1ec0524e13841a76f903b08237939a5b26f5ed
GeorgiYosifov <georgi_yosifov99@abv.bg>
2022-02-20 17:41:25 +0200

    Add table

--- ui/financial-calculator/src/views/credits.vue
+++ ui/financial-calculator/src/views/credits.vue
@@ -1,72 +1,76 @@
 <template>
-  <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          md="3"
-        >
-          <v-text-field
-            v-model="amount"
-            label="Размер на кредита *"
-          ></v-text-field>
-        </v-col>
-        <v-col
-          cols="12"
-          md="3"
-        >
-          <v-text-field
-            v-model="period"
-            label="Срок (месеци)*"
-          ></v-text-field>
-        </v-col>
-        <v-col
-          cols="12"
-          md="3"
-        >
-          <v-text-field
-            v-model="interest"
-            label="Лихва (%) *"
-          ></v-text-field>
-        </v-col>
-        <v-col 
+  <v-main>
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col
             cols="12"
             md="3"
-        >
-            <v-btn @click="submit">
-                submit
-            </v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+          >
+            <v-text-field
+              v-model="amount"
+              label="Размер на кредита *"
+            ></v-text-field>
+          </v-col>
+          <v-col
+            cols="12"
+            md="3"
+          >
+            <v-text-field
+              v-model="period"
+              label="Срок (месеци)*"
+            ></v-text-field>
+          </v-col>
+          <v-col
+            cols="12"
+            md="3"
+          >
+            <v-text-field
+              v-model="interest"
+              label="Лихва (%) *"
+            ></v-text-field>
+          </v-col>
+          <v-col 
+              cols="12"
+              md="3"
+          >
+              <v-btn @click="submit">
+                  submit
+              </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+    <v-data-table
+      :headers="headers"
+      :items="payments"
+      :items-per-page="10"
+      class="elevation-1"
+    ></v-data-table>
+  </v-main>
 </template>
 
 <script>
 import CalculatorService from '../common/calculator.service'
 
 export default {
-    name: "credits-view",
-    data: () => ({
-        valid: false,
-        amount: null,
-        period: null,
-        interest: null,
-    }),
-    // computed: {
-    //     form () {
-    //         return {
-    //         }
-    //     },
-    // },
-    methods: {
-        submit () {
-            CalculatorService.calculate({
-                amount: this.amount,
-                period: this.period,
-                interest: this.interest
-            })
-        },
-    }
+  name: "credits-view",
+  data: () => ({
+      valid: false,
+      amount: null,
+      period: null,
+      interest: null,
+  }),
+  methods: {
+    submit () {
+      CalculatorService.calculate({
+          amount: this.amount,
+          period: this.period,
+          interest: this.interest
+      }).then((response) => {
+        console.log(response);
+      })
+    },
+  }
 }
 </script>
