31bc0fd95d43bea6678ee49cd7f6ed632f64901e
GeorgiYosifov <georgi_yosifov99@abv.bg>
2022-02-20 19:27:38 +0200

    Fix table

--- ui/financial-calculator/src/views/credits.vue
+++ ui/financial-calculator/src/views/credits.vue
@@ -1,101 +1,109 @@
 <template>
-  <!-- <v-container> -->
-    <!-- <div>
-      <v-form v-model="valid">
-        <v-container>
-          <v-row>
-            <v-col
+  <v-main>
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="3"
+          >
+            <v-text-field
+              v-model="amount"
+              label="Размер на кредита *"
+            ></v-text-field>
+          </v-col>
+          <v-col
+            cols="12"
+            md="3"
+          >
+            <v-text-field
+              v-model="period"
+              label="Срок (месеци)*"
+            ></v-text-field>
+          </v-col>
+          <v-col
+            cols="12"
+            md="3"
+          >
+            <v-text-field
+              v-model="interest"
+              label="Лихва (%) *"
+            ></v-text-field>
+          </v-col>
+          <v-col 
               cols="12"
               md="3"
-            >
-              <v-text-field
-                v-model="amount"
-                label="Размер на кредита *"
-              ></v-text-field>
-            </v-col>
-            <v-col
-              cols="12"
-              md="3"
-            >
-              <v-text-field
-                v-model="period"
-                label="Срок (месеци)*"
-              ></v-text-field>
-            </v-col>
-            <v-col
-              cols="12"
-              md="3"
-            >
-              <v-text-field
-                v-model="interest"
-                label="Лихва (%) *"
-              ></v-text-field>
-            </v-col>
-            <v-col 
-                cols="12"
-                md="3"
-            >
-                <v-btn @click="submit">
-                    submit
-                </v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
-    </div> -->
-      <v-data-table
-        :headers="headers"
-        :items="payments"
-        :items-per-page="10"
-        class="elevation-1"
-      ></v-data-table>
-  <!-- </v-container> -->
+          >
+              <v-btn @click="submit">
+                  submit
+              </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+    <v-table v-show="payments.length">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Дата
+            </th>
+            <th class="text-left">
+              Месечна вноска
+            </th>
+            <th class="text-left">
+              Вноска главница
+            </th>
+            <th class="text-left">
+              Вноска лихва
+            </th>
+            <th class="text-left">
+              Остатък главница
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item in payments"
+            :key="item.date"
+          >
+            <td>{{ item.date }}</td>
+            <td>{{ item.monthlyPayment }}</td>
+            <td>{{ item.monthlyPrincipal }}</td>
+            <td>{{ item.monthlyInterest }}</td>
+            <td>{{ item.leftPrincipal }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-table>
+  </v-main>
 </template>
 
 <script>
-import CalculatorService from '../common/calculator.service'
+  import CalculatorService from '../common/calculator.service'
 
-export default {
-  name: "credits-view",
-  data: () => ({
-    valid: false,
-    amount: null,
-    period: null,
-    interest: null,
+  export default {
+    name: 'credits-view',
+    data () {
+      return {
+        valid: false,
+        amount: null,
+        period: null,
+        interest: null,
 
-    headers: [
-      { text: 'fwfwf', value: 'date' },
-      { text: 'vsve', value: 'monthlyPayment' },
-      { text: 'vvdv', value: 'monthlyPrincipal' },
-      { text: 'vedve', value: 'monthlyInterest' },
-      { text: 'vege', value: 'leftPrincipal' }
-    ],
-    payments: [
-      {
-        date: "goshr",
-        monthlyPayment: "1",
-        monthlyPrincipal: "2",
-        monthlyInterest: "3",
-        leftPrincipal: "4"
+        payments: [],
       }
-    ],
-  }),
-  // computed: {
-  //   getPayments() {
-  //     return this.payments;
-  //   }
-  // },
-  methods: {
-    submit () {
-      CalculatorService.calculate({
-          amount: this.amount,
-          period: this.period,
-          interest: this.interest
-      }).then((response) => {
-        this.payments = response.data.message.montlyCreditData;
-        console.log(this.payments);
-      })
+    },
+    methods: {
+      submit () {
+        CalculatorService.calculate({
+            amount: this.amount,
+            period: this.period,
+            interest: this.interest
+        }).then((response) => {
+          this.payments = response.data.message.montlyCreditData;
+        });
+      }
     },
   }
-}
 </script>
