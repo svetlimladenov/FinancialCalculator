8475db42d29d48c23e605503fab8b9bc8efaec51
svetlimladenov <svetlimladenov@gmail.com>
2022-02-21 12:31:58 +0200

    Call the http endpoint from the vue app

--- ui/financial-calculator/src/common/config.js
+++ ui/financial-calculator/src/common/config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://localhost:5001",
+  baseURL: "http://localhost:5000",
   headers: {
-    "Content-type": "application/json"
-  }
+    "Content-type": "application/json",
+  },
 });
